60c77e30a127f236297cf75671c240af64c292eb
zeppaman <zeppaman@altervista.org>
2015-11-16 19:43:45 +0100

    missing files in previous commit

---
+++ Wlog.TestApp/Test/TestIterator.cs
@@ -0,0 +1,49 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Wlog.TestApp.Test
+{
+    public class TestIterator
+    {
+        Logger l = LogManager.GetCurrentClassLogger();
+        public int RepeatCount { get; set; }
+
+        public List<TestInstance> Instances { get; set; }
+
+        public TestIterator()
+        {
+            RepeatCount = 10;
+            Instances = new List<TestInstance>();
+        }
+
+        public void DoTest()
+        { 
+            Stopwatch sw= new Stopwatch();
+            l.Info("TEST START");
+            foreach(TestInstance t in Instances)
+            {
+                l.Info("TEST START"+t.ToString());
+                sw.Reset();
+                sw.Start();
+                for(int i=0; i< RepeatCount;i++)
+                {
+
+                    
+                    t.Execute();
+                   
+                }
+                sw.Stop();
+                 double ms =   1000.0 * (double)sw.ElapsedTicks / Stopwatch.Frequency;
+                 double med=ms/RepeatCount;
+                 l.Info("TEST TIME" + ms+" avg:"+med);
+                 Thread.Sleep(10000);
+            }
+        }
+    }
+}
---
+++ Wlog.TestApp/Test/WlogTest.cs
@@ -0,0 +1,19 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wlog.TestApp.Test
+{
+    public class WlogTest : TestInstance
+    {
+        static Logger logger = LogManager.GetLogger("wlog.logger");
+
+        public void Execute()
+        {
+            logger.Info("THIS IS A SAMPLE MESSEGE TO LOG.");
+        }
+    }
+}
