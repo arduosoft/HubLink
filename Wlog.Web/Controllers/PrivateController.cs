336a30b8d1c59e66eec20c865b73aa5e6432ffb2
zeppaman <zeppaman@altervista.org>
2015-12-12 17:42:07 +0100

    missing files

---
+++ Wlog.Web/Controllers/PrivateController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wlog.Web.Models;
+using Wlog.Web.Code.Helpers;
+using Wlog.Web.Code.Classes;
+using System.Web.Security;
+using PagedList;
+
+namespace Wlog.Web.Controllers
+{
+    public class PrivateController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Logs(int? applicationId,string level,string sortOrder,string serchMessage,int? page, int? pageSize)
+        {
+
+            LogListModel mm = new LogListModel()
+            {
+                ApplicationId =applicationId,
+                Level = level,
+                SortOrder = sortOrder,
+                SerchMessage = serchMessage,               
+                
+            };
+            MembershipUser current = Membership.GetUser();
+            mm.Apps = UserHelper.GetAppsForUser(current.UserName);
+            mm.Apps.Insert(0, new Wlog.Models.ApplicationEntity() {                
+                ApplicationName="All application"
+            });
+
+
+            mm.Items = LogHelper.GetLogs(mm.ApplicationId, mm.SortOrder, mm.SerchMessage, 30, page??1);
+
+            
+           
+           
+            return View(mm);
+        }
+
+    }
+}
---
+++ Wlog.Web/Models/LogListModel.cs
@@ -0,0 +1,29 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Wlog.Models;
+using Wlog.Web.Code.Classes;
+
+namespace Wlog.Web.Models
+{
+    //Wlog.Web.Models.LogListModel
+    public class LogListModel
+    {
+
+        public int? ApplicationId { get; set; }
+        public string Level { get; set; }
+        public string SortOrder { get; set; }
+        public string SerchMessage { get; set; }
+        public List<ApplicationEntity> Apps { get; set; }
+
+       
+
+
+      
+        public IPagedList<LogEntity> Items { get; set; }
+
+    }
+}
