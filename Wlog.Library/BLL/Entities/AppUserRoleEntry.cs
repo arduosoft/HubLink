3d06fb486ac36b042b6d21de8474be079fc0be2b
zmn <zemmapan@altervista.org>
2016-02-20 12:52:35 +0100

    missing files

---
+++ Wlog.Library/BLL/Entities/AppUserRoleEntry.cs
@@ -0,0 +1,21 @@
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Wlog.BLL.Entities
+{
+    public class AppUserRoleEntity
+    {
+        [BsonId]
+        public virtual Guid Id { get; protected set; }
+        public virtual Guid ApplicationId { get; set; }
+        public virtual Guid UserId { get; set; }
+        public virtual Guid RoleId { get; set; }
+    }
+
+    
+}
---
+++ Wlog.Library/BLL/Entities/RolesEntry.cs
@@ -0,0 +1,20 @@
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Wlog.BLL.Entities
+{
+    public class RolesEntity
+    {
+        [BsonId]
+        public virtual Guid Id { get; set; }
+        public virtual string RoleName { get; set; }
+
+    }
+
+  
+}
