118f408aed5ee642bbda6754540e10aa4e2885c7
zmn <zemmapan@altervista.org>
2016-02-20 17:06:25 +0100

    missing file

---
+++ Wlog.Library/DAL/Nhibernate/Mappings/AppUserRoleMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using Wlog.BLL.Entities;
+
+namespace Wlog.Library.DAL.Nhibernate.Mappings
+{
+    public class AppUserRoleMap : ClassMapping<AppUserRoleEntity>
+    {
+        public AppUserRoleMap()
+        {
+            Table("WL_AppUserRole");
+            Schema("dbo");
+            Id(x => x.Id, map => map.Generator(Generators.Guid));
+            Property(x => x.ApplicationId, map => { map.Column("IdApplication");  });
+            Property(x => x.UserId, map => { map.Column("IdUser");  });
+            Property(x => x.RoleId, map => { map.Column("IdRole");  });
+        }
+    }
+}
